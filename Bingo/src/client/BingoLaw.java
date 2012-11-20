6e54ad1185994364de5e0e039fa90c4059cdcf58
Eduardo Christ <eduprankster@gmail.com>
2012-11-20 08:50:42 -0200

    .

--- Bingo/src/client/BingoLaw.java
+++ Bingo/src/client/BingoLaw.java
@@ -1,6 +1,5 @@
 package client;
 
-
 import java.awt.Color;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
@@ -18,6 +17,7 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+
 /*
  * @author Wagner Dias
  */
@@ -25,123 +25,119 @@ public class BingoLaw extends JPanel {
 
 	private ArrayList<JButton> botoes;
 	public static final String SERVER_HOSTNAME = "192.168.4.85";
-    public static final int SERVER_PORT = 2002;
-    static JLabel label = null;
-    static Sender sender = null;
-    static List<Integer> number = null;
+	public static final int SERVER_PORT = 2002;
+	static JLabel label = null;
+	static Sender sender = null;
+	static List<Integer> number = null;
 
 	public static void main(String[] args) {
-		
 
-		
-		//Sockets
+		// Sockets
 		BufferedReader in = null;
-        PrintWriter out = null;
-        
-        try {
-           // Connect to Bingo Law
-           Socket socket = new Socket(SERVER_HOSTNAME, SERVER_PORT);
-           in = new BufferedReader(
-               new InputStreamReader(socket.getInputStream()));
-           out = new PrintWriter(
-               new OutputStreamWriter(socket.getOutputStream()));
-           System.out.println("Connected to server " +
-              SERVER_HOSTNAME + ":" + SERVER_PORT);
-        } catch (IOException ioe) {
-           System.err.println("Can not establish connection to " +
-               SERVER_HOSTNAME + ":" + SERVER_PORT);
-           ioe.printStackTrace();
-           System.exit(-1);
-        }
- 
-        // Create and start Sender thread
-        sender = new Sender(out);
- 
-        try {
-           // Read messages from the server and print them
-            String message;
-           while ((message=in.readLine()) != null) {
-//               System.out.println(message);
-        	   if(message.startsWith("[")){
-         		   System.out.println(message);
-         		   number = new ArrayList<Integer>();
-         		   message = message.substring(1, message.length()-1);
-         		   String[] numbers = message.split(", ");
-         		   System.out.println(numbers[0]);
-         		   for(String s : numbers) {
-         			   number.add(Integer.parseInt(s));
-         			}
-         		   System.out.println(number);
-         			//Swing
-	   				BingoLaw gst = new BingoLaw();
-	   				JFrame f = new JFrame("Bingo Law");
-	   				f.getContentPane().add(gst);
-	   				f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	   				f.pack();
-	   				f.setVisible(true);
-        	   }else{
-        		   label.setText(message);
-        	   }
-           }
-        } catch (IOException ioe) {
-           System.err.println("Connection to server broken.");
-           ioe.printStackTrace();
-        }
+		PrintWriter out = null;
+
+		try {
+			// Connect to Bingo Law
+			Socket socket = new Socket(SERVER_HOSTNAME, SERVER_PORT);
+			in = new BufferedReader(new InputStreamReader(
+					socket.getInputStream()));
+			out = new PrintWriter(new OutputStreamWriter(
+					socket.getOutputStream()));
+			System.out.println("Connected to server " + SERVER_HOSTNAME + ":"
+					+ SERVER_PORT);
+		} catch (IOException ioe) {
+			System.err.println("Can not establish connection to "
+					+ SERVER_HOSTNAME + ":" + SERVER_PORT);
+			ioe.printStackTrace();
+			System.exit(-1);
+		}
+
+		// Create and start Sender thread
+		sender = new Sender(out);
+
+		try {
+			// Read messages from the server and print them
+			String message;
+			while ((message = in.readLine()) != null) {
+				// System.out.println(message);
+				if (message.startsWith("[")) {
+					System.out.println(message);
+					number = new ArrayList<Integer>();
+					message = message.substring(1, message.length() - 1);
+					String[] numbers = message.split(", ");
+					System.out.println(numbers[0]);
+					for (String s : numbers) {
+						number.add(Integer.parseInt(s));
+					}
+					System.out.println(number);
+					// Swing
+					BingoLaw gst = new BingoLaw();
+					JFrame f = new JFrame("Bingo Law");
+					f.getContentPane().add(gst);
+					f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+					f.pack();
+					f.setVisible(true);
+				} else {
+					label.setText(message);
+				}
+			}
+		} catch (IOException ioe) {
+			System.err.println("Connection to server broken.");
+			ioe.printStackTrace();
+		}
 	}
 
 	private static final long serialVersionUID = 4221317269750527161L;
 
 	public BingoLaw() {
-		
+
 		botoes = new ArrayList<JButton>(30);
 		setLayout(new GridLayout(7, 5));
 		String bingo = "BINGO";
 		for (int i = 0; i < 5; i++) {
-		final JButton buttoncol = new JButton(String.valueOf(bingo.charAt(i)));
-		buttoncol.setBackground(Color.white);
-		add(buttoncol);
-		botoes.add(buttoncol);
-		buttoncol.addActionListener(new ActionListener() {
-			
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				JButton b = (JButton)e.getSource();
-				sender.send("coluna " + b.getText());
-				
-			}
-		});
+			final JButton buttoncol = new JButton(String.valueOf(bingo
+					.charAt(i)));
+			buttoncol.setBackground(Color.white);
+			add(buttoncol);
+			botoes.add(buttoncol);
+			buttoncol.addActionListener(new ActionListener() {
+
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					JButton b = (JButton) e.getSource();
+					sender.send("coluna " + b.getText());
+
+				}
+			});
 		}
-		
-		
-		
+
 		for (int i = 5; i < 29; i++) {
 
 			if (i == 17) {
-				//JButton button = new JButton("");
+				// JButton button = new JButton("");
 				label = new JLabel("Sorteio");
 				add(label);
-				//button.setEnabled(false);
+				// button.setEnabled(false);
 			}
 
-
-			JButton button = new JButton(""+number.get(i-5));
+			JButton button = new JButton("" + number.get(i - 5));
 			button.setBackground(Color.white);
 			add(button);
 			botoes.add(button);
 			button.addActionListener(new ActionListener() {
-				
+
 				@Override
 				public void actionPerformed(ActionEvent e) {
-					// TODO Auto-generated method stub
-				//System.out.println(e);
-				
-				JButton b = (JButton)e.getSource();
-				if(b.getBackground().equals(Color.green)){
-					b.setBackground(Color.white);
-				}else{
-					b.setBackground(Color.green);
-				}
-				System.out.println();
+					
+					// System.out.println(e);
+
+					JButton b = (JButton) e.getSource();
+					if (b.getBackground().equals(Color.green)) {
+						b.setBackground(Color.white);
+					} else {
+						b.setBackground(Color.green);
+					}
+					System.out.println();
 				}
 			});
 		}
@@ -149,14 +145,12 @@ public class BingoLaw extends JPanel {
 		add(buttonBingo);
 		botoes.add(buttonBingo);
 		buttonBingo.addActionListener(new ActionListener() {
-			
+
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				sender.send("bingo");
 			}
 		});
 	}
-
-
 
 }
--- Bingo/src/client/Sender.java
+++ Bingo/src/client/Sender.java
@@ -2,23 +2,20 @@ package client;
 
 import java.io.PrintWriter;
 
-class Sender
-{
-private PrintWriter mOut;
- 
-public Sender(PrintWriter aOut)
-{
-        mOut = aOut;
-}
- 
-    /**
-     * Until interrupted reads messages from the standard input (keyboard)
-     * and sends them to the chat server through the socket.
-     */
-public void send(String message)
-{
+class Sender {
+	private PrintWriter mOut;
 
-mOut.println(message);
-mOut.flush();
-}
+	public Sender(PrintWriter aOut) {
+		mOut = aOut;
+	}
+
+	/**
+	 * Until interrupted reads messages from the standard input (keyboard) and
+	 * sends them to the chat server through the socket.
+	 */
+	public void send(String message) {
+
+		mOut.println(message);
+		mOut.flush();
+	}
 }
